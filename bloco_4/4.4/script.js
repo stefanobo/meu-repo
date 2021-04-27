9982e93d7a59a36c7440b329a31fba92c4ffedaa
Stefano <stefanooliveira1@gmail.com>
2021-04-27 19:10:26 -0300

    resolve exs 1 e 2 da Parte II

--- bloco_4/4.4/script.js
+++ bloco_4/4.4/script.js
@@ -48,7 +48,59 @@ let names = {
   info.recorrente = 'Sim';
 
   console.log(info.recorrente);
-
+//ex3
   for (let key in info) {
         console.log(key);
   }
+//ex4
+  for (let key in info) {
+    console.log(info[key]);
+}
+//ex5
+
+let info = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell's Four Color Comics #178',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+
+  // Parte II - Funções
+
+  //ex1
+
+
+  function verificaPalindrome (palavra) {
+      let palavraTestada = palavra.split("").reverse().join("");
+      if (palavraTestada === palavra) {
+          console.log(true);
+      } else {
+          console.log(false);
+      }
+  }
+
+  let palavraAVerificar = 'bicicleta';
+
+  verificaPalindrome (palavraAVerificar);
+
+  //ex 2
+
+  let arrayTeste = [2, 3, 6, 7, 10, 1];
+
+  function retornaMaiorValor (arrayAlvo) {
+      let maiorNumero;
+      let indiceMaior;
+      for (let index = 0; index < arrayAlvo.length; index += 1) {
+        if (arrayAlvo[index] > arrayAlvo[index + 1]) {
+            maiorNumero = arrayAlvo[index]
+        }
+      }
+      for (let index2 = 0; index2 < arrayAlvo.length; index2 += 1) {
+          if (maiorNumero === arrayAlvo[index2]) {
+            indiceMaior = index2
+          }
+      }
+      return indiceMaior;
+  };
+
+  console.log(retornaMaiorValor(arrayTeste))
