3197c1eb22927a55e9368d107fe07544d202c63c
Stefano <stefanooliveira1@gmail.com>
2021-05-21 20:09:29 -0300

    termina parte 2

---
+++ exercises/7.3/p2ex4.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+const findTheNeedle = (arr, word) => {
+    let valor = -1;
+
+    for (key in arr) {
+        if (word === arr[key]) {
+            valor = parseInt(key, 10);
+        }
+    }
+    return valor;
+}
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
